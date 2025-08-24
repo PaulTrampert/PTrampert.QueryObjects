5a7f63eafd0bcfafff6ffe0b9a4de214d24275c7
Paul Trampert <paul.trampert@ptrampert.com>
2025-08-24 11:37:01 -0400

    Fix doc comment.

--- PTrampert.QueryObjects/QueryableExtensions.cs
+++ PTrampert.QueryObjects/QueryableExtensions.cs
@@ -8,9 +8,8 @@ public static class QueryableExtensions
 {
     /// <summary>
     /// Filters a queryable based on the properties of a query object. By default, properties are matched by name, and
-    /// all non-null properties are included in the filter. You can override this behavior by using
-    /// <see cref="Attributes.QueryAttribute"/>s on the properties of the query object. Additionally, this behavior
-    /// can by extended by implementing <see cref="IQueryObject{T}"/> on the query object.
+    /// only properties annotated with <see cref="Attributes.QueryAttribute"/> are included in the filter. This behavior
+    /// can be extended by implementing <see cref="IQueryObject{T}"/> on the query object.
     /// </summary>
     /// <param name="queryable">The queryable to be fitered.</param>
     /// <param name="queryObject">The query object used to build the filter.</param>
