54a292810ee744d2e7e7eb8468cdad10e2e79229
Paul Trampert <paul.trampert@ptrampert.com>
2025-08-24 14:19:49 -0400

    Fix missing doc comment

--- PTrampert.QueryObjects/QueryableExtensions.cs
+++ PTrampert.QueryObjects/QueryableExtensions.cs
@@ -23,6 +23,15 @@ namespace PTrampert.QueryObjects
             return Queryable.Where(queryable, expression);
         }
 
+        /// <summary>
+        /// Filters a enumerable based on the properties of a query object. By default, properties are matched by name, and
+        /// only properties annotated with <see cref="Attributes.QueryAttribute"/> are included in the filter. This behavior
+        /// can be extended by implementing <see cref="IQueryObject{T}"/> on the query object.
+        /// </summary>
+        /// <param name="enumerable">The enumerable to be fitered.</param>
+        /// <param name="queryObject">The query object used to build the filter.</param>
+        /// <typeparam name="T">The type being filtered.</typeparam>
+        /// <returns>The filtered <see cref="IEnumerable{T}"/></returns>
         public static IEnumerable<T> Where<T>(this IEnumerable<T> enumerable, object queryObject)
         {
             return enumerable.AsQueryable().Where(queryObject);
