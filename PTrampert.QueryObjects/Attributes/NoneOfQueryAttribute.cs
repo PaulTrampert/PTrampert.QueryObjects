7b7725349a3ce7f2c936cb735aa17f31f765ea70
Paul Trampert <5550942+PaulTrampert@users.noreply.github.com>
2025-09-06 13:18:12 -0400

    (MINOR) Add NoneOfQueryAttribute (#11)

---
+++ PTrampert.QueryObjects/Attributes/NoneOfQueryAttribute.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace PTrampert.QueryObjects.Attributes
+{
+    /// <summary>
+    /// Checks if the target property is not contained in the query property's enumerable value.
+    /// </summary>
+    public class NoneOfQueryAttribute : QueryAttribute
+    {
+        public NoneOfQueryAttribute(string targetProperty = null)
+            : base(targetProperty)
+        {
+        }
+        
+        /// <inheritdoc />
+        public override Expression BuildExpression(object queryObject, PropertyInfo queryProperty, ParameterExpression targetParameter,
+            PropertyInfo targetProperty)
+        {
+            if (!(queryProperty.GetValue(queryObject) is IEnumerable queryValue))
+                return null;
+
+            var elementType = targetProperty.PropertyType;
+            var containsMethod = typeof(Enumerable).GetMethods(BindingFlags.Static | BindingFlags.Public)
+                .Single(m => m.Name == nameof(Enumerable.Contains) && m.GetParameters().Length == 2)
+                .MakeGenericMethod(elementType);
+
+            var constant = Expression.Constant(queryValue);
+            var propertyAccess = Expression.Property(targetParameter, targetProperty);
+            return Expression.Not(Expression.Call(containsMethod, constant, propertyAccess));
+        }
+    }
+}
