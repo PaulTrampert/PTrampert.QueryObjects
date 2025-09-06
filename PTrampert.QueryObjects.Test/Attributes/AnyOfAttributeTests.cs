c984d0a1c87f9bbf2c63e42e4c6fdc980015089d
Paul Trampert <paul.trampert@ptrampert.com>
2025-09-06 11:24:53 -0400

    Implement AnyOf attribute that can be applied to an IEnumerable query property

---
+++ PTrampert.QueryObjects.Test/Attributes/AnyOfAttributeTests.cs
@@ -0,0 +1,44 @@
+using PTrampert.QueryObjects.Attributes;
+
+namespace PTrampert.QueryObjects.Test.Attributes;
+
+public class AnyOfAttributeTests
+{
+    private record AnyOfTestTarget
+    {
+        public Guid Id { get; init; }
+    }
+    
+    private record AnyOfTestQuery
+    {
+        [AnyOf(nameof(AnyOfTestTarget.Id))]
+        public IEnumerable<Guid>? Ids { get; init; }
+    }
+    
+    
+    [Test]
+    public void AnyOfAttribute_SelectsRecordsWithAnyOfTheSpecifiedValues()
+    {
+        // Arrange
+        var id1 = Guid.NewGuid();
+        var id2 = Guid.NewGuid();
+        var id3 = Guid.NewGuid();
+        
+        var data = new List<AnyOfTestTarget>
+        {
+            new() { Id = id1 },
+            new() { Id = id2 },
+            new() { Id = id3 }
+        };
+
+        var queryObject = new AnyOfTestQuery
+        {
+            Ids = new[] { id1, id3 }
+        };
+
+        // Act
+        var result = data.Where(queryObject);
+        
+        Assert.That(result, Is.EqualTo([data[0], data[2]]));
+    }
+}
