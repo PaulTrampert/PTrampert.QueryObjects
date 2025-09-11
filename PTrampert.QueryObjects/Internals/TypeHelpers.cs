e3eb73c2c5b807ebd10596ea770b1429c8d537c7
Paul Trampert <paul.trampert@ptrampert.com>
2025-09-10 22:35:13 -0400

    Add caches for Intersect, Any, and Contains methods

--- PTrampert.QueryObjects/Internals/TypeHelpers.cs
+++ PTrampert.QueryObjects/Internals/TypeHelpers.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Concurrent;
+using System.Linq;
+using System.Reflection;
 
 namespace PTrampert.QueryObjects.Internals
 {
@@ -22,6 +24,36 @@ namespace PTrampert.QueryObjects.Internals
                 
                 return cType.IsGenericType ? cType.GetGenericArguments()[0] : typeof(object);
             });
+        }
+        
+        private static readonly ConcurrentDictionary<Type, MethodInfo> ElementTypeToContainsMethod = new();
+
+        public static MethodInfo GetContainsMethod(this Type elementType)
+        {
+            return ElementTypeToContainsMethod.GetOrAdd(elementType, eType =>
+                typeof(Enumerable).GetMethods(BindingFlags.Static | BindingFlags.Public)
+                    .Single(m => m.Name == nameof(Enumerable.Contains) && m.GetParameters().Length == 2)
+                    .MakeGenericMethod(eType));
+        }
+        
+        private static readonly ConcurrentDictionary<Type, MethodInfo> ElementTypeToIntersectMethod = new();
+        
+        public static MethodInfo GetIntersectMethod(this Type elementType)
+        {
+            return ElementTypeToIntersectMethod.GetOrAdd(elementType, eType =>
+                typeof(Enumerable).GetMethods(BindingFlags.Static | BindingFlags.Public)
+                    .Single(m => m.Name == nameof(Enumerable.Intersect) && m.GetParameters().Length == 2)
+                    .MakeGenericMethod(eType));
+        }
+        
+        private static readonly ConcurrentDictionary<Type, MethodInfo> ElementTypeToAnyMethod = new();
+        
+        public static MethodInfo GetAnyMethod(this Type elementType)
+        {
+            return ElementTypeToAnyMethod.GetOrAdd(elementType, eType =>
+                typeof(Enumerable).GetMethods(BindingFlags.Static | BindingFlags.Public)
+                    .Single(m => m.Name == nameof(Enumerable.Any) && m.GetParameters().Length == 1)
+                    .MakeGenericMethod(eType));
         }
     }
 }
